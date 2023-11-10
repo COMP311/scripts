fe98a3c826d01cccf21e4e8793ea6ee14dbc8cc5
Jesse Wei <jesse@cs.unc.edu>
2023-11-10 12:14:29 -0500

    Nits (#1)
    * Modify gitattributes, remove instances of \`
    
    A student copy pasted the `
    
    * Create SHELL_RC_FILE if DNE

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ if ! command -v java &> /dev/null; then
         sdk default java $JAVA_VER
         if ! sdk current java | grep -q "$JAVA_VER"; then
             printf "This script was unable to set your Java version to $JAVA_VER using SDKMAN! Please install java yourself and re-run this script.
-Halting.\n"
+            Halting.\n"
         fi
     else
         echo "Halting installation. Please install java yourself and re-run this script."
@@ -44,7 +44,7 @@ if [[ "$FLAG_SAPSIM" != "no_sapsim" ]]; then
         echo "You do not have Python 3.9+ installed (i.e., python and python3 commands both not found). Please install Python 3.9+ (required for SAPsim tool) and re-run this script."
         echo "Exiting."
         exit 1
-    elif command -v python &> /dev/null; then
+        elif command -v python &> /dev/null; then
         python -m pip install --upgrade pip
         python -m pip install SAPsim
     else
@@ -85,19 +85,23 @@ JARS=(
 # This script is always run via bash
 # But $SHELL will be user's shell (e.g., /bin/zsh) even so
 if [[ $SHELL = *"bash"* ]]; then
-	SHELL_RC_FILE=$HOME/.bashrc
-elif [[ $SHELL = *"zsh"* ]]; then
-	SHELL_RC_FILE=$HOME/.zshrc
-# Extreme edge case
+    SHELL_RC_FILE=$HOME/.bashrc
+    elif [[ $SHELL = *"zsh"* ]]; then
+    SHELL_RC_FILE=$HOME/.zshrc
+    # Extreme edge case
 else
     printf "\nJAR files have been downloaded to $INSTALL_DIR.
-    
+
 However, this script cannot create aliases for the JAR files because your \$SHELL is not bash or zsh,
 so I don't know what your shell's equivalent to .bashrc or .zshrc is.
 You can alias the command yourself, if you'd like.
 I assume that since you're not using bash nor zsh, you know how to do this.
-Otherwise, the .jar has already been downloaded and can be run however you normally run a .jar file.\n"
+    Otherwise, the .jar has already been downloaded and can be run however you normally run a .jar file.\n"
     exit 1
+fi
+
+if [[ ! -f "$SHELL_RC_FILE" ]]; then
+    touch "$SHELL_RC_FILE"
 fi
 
 SHELL_RC_FILE_OWNER=$(ls -l $SHELL_RC_FILE | awk '{print $3}')
@@ -105,7 +109,7 @@ SHELL_RC_FILE_OWNER=$(ls -l $SHELL_RC_FILE | awk '{print $3}')
 if [[ "$SHELL_RC_FILE_OWNER" != "$USER" ]]; then
     echo "ERROR: Owner of $SHELL_RC_FILE is not you ($USER)."
     # Group is left out, so the group becomes the default group for $USER
-    echo "Run \`sudo chown $USER: $SHELL_RC_FILE\` to change ownership, then re-run this script."
+    echo "Run sudo chown $USER: $SHELL_RC_FILE to change ownership, then re-run this script."
     echo "Exiting."
     exit 1
 fi
@@ -118,7 +122,7 @@ for kv in "${JARS[@]}" ; do
 done
 
 printf "\nJAR files have been downloaded to $INSTALL_DIR, and aliases for launching the JAR files have been created.\n\n"
-printf "**Run \`source $SHELL_RC_FILE\` to apply changes to your current shell session; otherwise, restart your terminal.** Then,\n\n"
+printf "**Run source $SHELL_RC_FILE to apply changes to your current shell session; otherwise, restart your terminal.** Then,\n\n"
 
 USAGE_FILE=$INSTALL_DIR/usage.txt
 USAGE=""
@@ -126,9 +130,9 @@ USAGE=""
 for kv in "${JARS[@]}" ; do
     COMMAND=${kv%%:*}
     JAR=${kv#*:}
-    USAGE+="Run \`$COMMAND\` to launch $JAR\n"
+    USAGE+="Run $COMMAND to launch $JAR\n"
 done
-USAGE+="For Digital, run \`digital file.dig\` to directly open file.dig on launch. However, this does not work for MARS.\n"
+USAGE+="For Digital, run digital file.dig to directly open file.dig on launch. However, this does not work for MARS.\n"
 if [[ "$FLAG_SAPSIM" != "no_sapsim" ]]; then
     USAGE+="SAPsim was installed via pip.\n"
 fi
@@ -136,4 +140,4 @@ fi
 printf "$USAGE" | tee $USAGE_FILE
 
 echo "alias help-comp311=\"cat $USAGE_FILE\"" >> "$SHELL_RC_FILE"
-echo "To view this message again, run \`help-comp311\`."
+echo "To view this message again, run help-comp311."
